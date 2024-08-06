1bd7ea78de6cb1e107e114661a3b1a488dbc0986
StanleyMurigi <stannjoroge643@gmail.com>
2024-08-06 20:50:17 +0300

    correcting the import error of the user defined package

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,7 +2,7 @@
 import asyncio
 import time
 from typing import List
-from 1_async_comprehension import async_comprehension
+async_comprehension = __import__('1_async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """
