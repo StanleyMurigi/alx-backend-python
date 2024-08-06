89b5cf5abf3793a5d1227fe65750f4d305b4364f
StanleyMurigi <stannjoroge643@gmail.com>
2024-08-06 20:47:30 +0300

    correcting the import error of the user defined package

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from typing import List
 import asyncio
-from 0_async_generator import async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """
